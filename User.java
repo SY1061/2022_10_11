26541593622ff9ad6fe7382a508438ff830b2d32
김선영 <godfks7636@naver.com>
2022-10-11 10:16:11 +0900

    10_11 User 커밋

---
+++ User.java
@@ -0,0 +1,17 @@
+public class User {
+    final String email;
+    final String phoneNumber;
+
+    public User(String email, String phoneNumber){
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+}
